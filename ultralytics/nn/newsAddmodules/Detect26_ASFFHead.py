7a7882c7b9321cfd92a468c47dcc1234a5a86e99
UltralyticsAssistant <web@ultralytics.com>
2026-05-01 06:19:44 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/newsAddmodules/Detect26_ASFFHead.py
+++ ultralytics/nn/newsAddmodules/Detect26_ASFFHead.py
@@ -1,13 +1,19 @@
+from __future__ import annotations
+
+import copy
 import math
-from ultralytics.utils.tal import dist2bbox, make_anchors
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import copy
+
+from ultralytics.utils.tal import dist2bbox, make_anchors
+
 # 论文地址：https://arxiv.org/abs/1911.09516
- 
-__all__ = ['Detect26_ASFFHead']
- 
+
+__all__ = ["Detect26_ASFFHead"]
+
+
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
     """Pad to 'same' shape outputs."""
     if d > 1:
@@ -15,33 +21,34 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
- 
- 
+
+
 class Conv(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
     default_act = nn.SiLU()  # default activation
- 
+
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
- 
+
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
         return self.act(self.bn(self.conv(x)))
- 
+
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
         return self.act(self.conv(x))
- 
+
+
 class DFL(nn.Module):
+    """Integral module of Distribution Focal Loss (DFL). Proposed in Generalized Focal Loss
+    https://ieeexplore.ieee.org/document/9792391.
     """
-    Integral module of Distribution Focal Loss (DFL).
-    Proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
-    """
- 
+
     def __init__(self, c1=16):
         """Initialize a convolutional layer with a given number of input channels."""
         super().__init__()
@@ -49,17 +56,21 @@ class DFL(nn.Module):
         x = torch.arange(c1, dtype=torch.float)
         self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
         self.c1 = c1
- 
+
     def forward(self, x):
         """Applies a transformer layer on input tensor 'x' and returns a tensor."""
-        b, c, a = x.shape  # batch, channels, anchors
+        b, _c, a = x.shape  # batch, channels, anchors
         return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
+
+
 class DWConv(Conv):
     """Depth-wise convolution."""
- 
+
     def __init__(self, c1, c2, k=1, s=1, d=1, act=True):  # ch_in, ch_out, kernel, stride, dilation, activation
         """Initialize Depth-wise convolution with given parameters."""
         super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), d=d, act=act)
+
+
 class Detect26_ASFFHead(nn.Module):
     dynamic = False  # force grid reconstruction
     export = False  # export mode
@@ -70,9 +81,10 @@ class Detect26_ASFFHead(nn.Module):
     strides = torch.empty(0)  # init
     legacy = False  # backward compatibility for v3/v5/v8/v9 models
     xyxy = False  # xyxy or xywh output
- 
+
     def __init__(self, nc: int = 80, reg_max=16, end2end=False, ch: tuple = ()):
         """Initialize the YOLO detection layer with specified number of classes and channels.
+
         Args:
             nc (int): Number of classes.
             reg_max (int): Maximum number of DFL channels.
@@ -108,22 +120,22 @@ class Detect26_ASFFHead(nn.Module):
         if end2end:
             self.one2one_cv2 = copy.deepcopy(self.cv2)
             self.one2one_cv3 = copy.deepcopy(self.cv3)
- 
+
     @property
     def one2many(self):
         """Returns the one-to-many head components, here for v5/v5/v8/v9/11 backward compatibility."""
         return dict(box_head=self.cv2, cls_head=self.cv3)
- 
+
     @property
     def one2one(self):
         """Returns the one-to-one head components."""
         return dict(box_head=self.one2one_cv2, cls_head=self.one2one_cv3)
- 
+
     @property
     def end2end(self):
         """Checks if the model has one2one for v5/v5/v8/v9/11 backward compatibility."""
         return hasattr(self, "one2one")
- 
+
     def forward_head(
         self, x: list[torch.Tensor], box_head: torch.nn.Module = None, cls_head: torch.nn.Module = None
     ) -> dict[str, torch.Tensor]:
@@ -134,7 +146,7 @@ class Detect26_ASFFHead(nn.Module):
         boxes = torch.cat([box_head[i](x[i]).view(bs, 4 * self.reg_max, -1) for i in range(self.nl)], dim=-1)
         scores = torch.cat([cls_head[i](x[i]).view(bs, self.nc, -1) for i in range(self.nl)], dim=-1)
         return dict(boxes=boxes, scores=scores, feats=x)
- 
+
     def forward(
         self, x: list[torch.Tensor]
     ) -> dict[str, torch.Tensor] | torch.Tensor | tuple[torch.Tensor, dict[str, torch.Tensor]]:
@@ -154,25 +166,27 @@ class Detect26_ASFFHead(nn.Module):
         if self.end2end:
             y = self.postprocess(y.permute(0, 2, 1))
         return y if self.export else (y, preds)
- 
+
     def _inference(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Decode predicted bounding boxes and class probabilities based on multiple-level feature maps.
+
         Args:
             x (dict[str, torch.Tensor]): List of feature maps from different detection layers.
+
         Returns:
             (torch.Tensor): Concatenated tensor of decoded bounding boxes and class probabilities.
         """
         # Inference path
         dbox = self._get_decode_boxes(x)
         return torch.cat((dbox, x["scores"].sigmoid()), 1)
- 
+
     def _get_decode_boxes(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Get decoded boxes based on anchors and strides."""
         shape = x["feats"][0].shape  # BCHW
         if self.format != "imx" and (self.dynamic or self.shape != shape):
             self.anchors, self.strides = (a.transpose(0, 1) for a in make_anchors(x["feats"], self.stride, 0.5))
             self.shape = shape
- 
+
         boxes = x["boxes"]
         if self.export and self.format in {"tflite", "edgetpu"}:
             # Precompute normalization factor to increase numerical stability
@@ -185,7 +199,7 @@ class Detect26_ASFFHead(nn.Module):
         else:
             dbox = self.decode_bboxes(self.dfl(boxes), self.anchors.unsqueeze(0)) * self.strides
         return dbox
- 
+
     def bias_init(self):
         """Initialize Detect() biases, WARNING: requires stride availability."""
         for i, (a, b) in enumerate(zip(self.one2many["box_head"], self.one2many["cls_head"])):  # from
@@ -199,7 +213,7 @@ class Detect26_ASFFHead(nn.Module):
                 b[-1].bias.data[: self.nc] = math.log(
                     5 / self.nc / (640 / self.stride[i]) ** 2
                 )  # cls (.01 objects, 80 classes, 640 img)
- 
+
     def decode_bboxes(self, bboxes: torch.Tensor, anchors: torch.Tensor, xywh: bool = True) -> torch.Tensor:
         """Decode bounding boxes from predictions."""
         return dist2bbox(
@@ -208,12 +222,14 @@ class Detect26_ASFFHead(nn.Module):
             xywh=xywh and not self.end2end and not self.xyxy,
             dim=1,
         )
- 
+
     def postprocess(self, preds: torch.Tensor) -> torch.Tensor:
         """Post-processes YOLO model predictions.
+
         Args:
             preds (torch.Tensor): Raw predictions with shape (batch_size, num_anchors, 4 + nc) with last dimension
                 format [x, y, w, h, class_probs].
+
         Returns:
             (torch.Tensor): Processed predictions with shape (batch_size, min(max_det, num_anchors), 6) and last
                 dimension format [x, y, w, h, max_class_prob, class_index].
@@ -222,12 +238,14 @@ class Detect26_ASFFHead(nn.Module):
         scores, conf, idx = self.get_topk_index(scores, self.max_det)
         boxes = boxes.gather(dim=1, index=idx.repeat(1, 1, 4))
         return torch.cat([boxes, scores, conf], dim=-1)
- 
+
     def get_topk_index(self, scores: torch.Tensor, max_det: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """Get top-k indices from scores.
+
         Args:
             scores (torch.Tensor): Scores tensor with shape (batch_size, num_anchors, num_classes).
             max_det (int): Maximum detections per image.
+
         Returns:
             (torch.Tensor, torch.Tensor, torch.Tensor): Top scores, class indices, and filtered indices.
         """
@@ -240,69 +258,50 @@ class Detect26_ASFFHead(nn.Module):
         scores, index = scores.flatten(1).topk(k)
         idx = ori_index[torch.arange(batch_size)[..., None], index // nc]  # original index
         return scores[..., None], (index % nc)[..., None].float(), idx
- 
+
     def fuse(self) -> None:
         """Remove the one2many head for inference optimization."""
         self.cv2 = self.cv3 = None
- 
- 
- 
+
+
 class ASFFV5(nn.Module):
     def __init__(self, level, ch, multiplier=1, rfb=False, vis=False, act_cfg=True):
+        """ASFF version for YoloV5 . different than YoloV3 multiplier should be 1, 0.5 which means, the channel of ASFF
+        can be 512, 256, 128 -> multiplier=1 256, 128, 64 -> multiplier=0.5 For even smaller, you need change
+        code manually.
         """
-        ASFF version for YoloV5 .
-        different than YoloV3
-        multiplier should be 1, 0.5 which means, the channel of ASFF can be
-        512, 256, 128 -> multiplier=1
-        256, 128, 64 -> multiplier=0.5
-        For even smaller, you need change code manually.
-        """
-        super(ASFFV5, self).__init__()
+        super().__init__()
         self.level = level
-        self.dim = [int(ch[2] * multiplier), int(ch[1] * multiplier),
-                    int(ch[0] * multiplier)]
+        self.dim = [int(ch[2] * multiplier), int(ch[1] * multiplier), int(ch[0] * multiplier)]
         # print(self.dim)
- 
+
         self.inter_dim = self.dim[self.level]
         if level == 0:
             self.stride_level_1 = Conv(int(ch[1] * multiplier), self.inter_dim, 3, 2)
- 
+
             self.stride_level_2 = Conv(int(ch[0] * multiplier), self.inter_dim, 3, 2)
- 
-            self.expand = Conv(self.inter_dim, int(
-                ch[2] * multiplier), 3, 1)
+
+            self.expand = Conv(self.inter_dim, int(ch[2] * multiplier), 3, 1)
         elif level == 1:
-            self.compress_level_0 = Conv(
-                int(ch[2] * multiplier), self.inter_dim, 1, 1)
-            self.stride_level_2 = Conv(
-                int(ch[0] * multiplier), self.inter_dim, 3, 2)
+            self.compress_level_0 = Conv(int(ch[2] * multiplier), self.inter_dim, 1, 1)
+            self.stride_level_2 = Conv(int(ch[0] * multiplier), self.inter_dim, 3, 2)
             self.expand = Conv(self.inter_dim, int(ch[1] * multiplier), 3, 1)
         elif level == 2:
-            self.compress_level_0 = Conv(
-                int(ch[2] * multiplier), self.inter_dim, 1, 1)
-            self.compress_level_1 = Conv(
-                int(ch[1] * multiplier), self.inter_dim, 1, 1)
-            self.expand = Conv(self.inter_dim, int(
-                ch[0] * multiplier), 3, 1)
- 
+            self.compress_level_0 = Conv(int(ch[2] * multiplier), self.inter_dim, 1, 1)
+            self.compress_level_1 = Conv(int(ch[1] * multiplier), self.inter_dim, 1, 1)
+            self.expand = Conv(self.inter_dim, int(ch[0] * multiplier), 3, 1)
+
         # when adding rfb, we use half number of channels to save memory
         compress_c = 8 if rfb else 16
-        self.weight_level_0 = Conv(
-            self.inter_dim, compress_c, 1, 1)
-        self.weight_level_1 = Conv(
-            self.inter_dim, compress_c, 1, 1)
-        self.weight_level_2 = Conv(
-            self.inter_dim, compress_c, 1, 1)
- 
-        self.weight_levels = Conv(
-            compress_c * 3, 3, 1, 1)
+        self.weight_level_0 = Conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_1 = Conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_2 = Conv(self.inter_dim, compress_c, 1, 1)
+
+        self.weight_levels = Conv(compress_c * 3, 3, 1, 1)
         self.vis = vis
- 
+
     def forward(self, x):  # l,m,s
-        """
-        # 128, 256, 512
-        512, 256, 128
-        from small -> large
+        """# 128, 256, 512 512, 256, 128 from small -> large.
         """
         x_level_0 = x[2]  # l
         x_level_1 = x[1]  # m
@@ -313,24 +312,20 @@ class ASFFV5(nn.Module):
         if self.level == 0:
             level_0_resized = x_level_0
             level_1_resized = self.stride_level_1(x_level_1)
-            level_2_downsampled_inter = F.max_pool2d(
-                x_level_2, 3, stride=2, padding=1)
+            level_2_downsampled_inter = F.max_pool2d(x_level_2, 3, stride=2, padding=1)
             level_2_resized = self.stride_level_2(level_2_downsampled_inter)
         elif self.level == 1:
             level_0_compressed = self.compress_level_0(x_level_0)
-            level_0_resized = F.interpolate(
-                level_0_compressed, scale_factor=2, mode='nearest')
+            level_0_resized = F.interpolate(level_0_compressed, scale_factor=2, mode="nearest")
             level_1_resized = x_level_1
             level_2_resized = self.stride_level_2(x_level_2)
         elif self.level == 2:
             level_0_compressed = self.compress_level_0(x_level_0)
-            level_0_resized = F.interpolate(
-                level_0_compressed, scale_factor=4, mode='nearest')
+            level_0_resized = F.interpolate(level_0_compressed, scale_factor=4, mode="nearest")
             x_level_1_compressed = self.compress_level_1(x_level_1)
-            level_1_resized = F.interpolate(
-                x_level_1_compressed, scale_factor=2, mode='nearest')
+            level_1_resized = F.interpolate(x_level_1_compressed, scale_factor=2, mode="nearest")
             level_2_resized = x_level_2
- 
+
         # print('level: {}, l1_resized: {}, l2_resized: {}'.format(self.level,
         #      level_1_resized.shape, level_2_resized.shape))
         level_0_weight_v = self.weight_level_0(level_0_resized)
@@ -339,28 +334,30 @@ class ASFFV5(nn.Module):
         # print('level_0_weight_v: ', level_0_weight_v.shape)
         # print('level_1_weight_v: ', level_1_weight_v.shape)
         # print('level_2_weight_v: ', level_2_weight_v.shape)
- 
-        levels_weight_v = torch.cat(
-            (level_0_weight_v, level_1_weight_v, level_2_weight_v), 1)
+
+        levels_weight_v = torch.cat((level_0_weight_v, level_1_weight_v, level_2_weight_v), 1)
         levels_weight = self.weight_levels(levels_weight_v)
         levels_weight = F.softmax(levels_weight, dim=1)
- 
-        fused_out_reduced = level_0_resized * levels_weight[:, 0:1, :, :] + \
-                            level_1_resized * levels_weight[:, 1:2, :, :] + \
-                            level_2_resized * levels_weight[:, 2:, :, :]
- 
+
+        fused_out_reduced = (
+            level_0_resized * levels_weight[:, 0:1, :, :]
+            + level_1_resized * levels_weight[:, 1:2, :, :]
+            + level_2_resized * levels_weight[:, 2:, :, :]
+        )
+
         out = self.expand(fused_out_reduced)
- 
+
         if self.vis:
             return out, levels_weight, fused_out_reduced.sum(dim=1)
         else:
             return out
- 
+
+
 if __name__ == "__main__":
     image1 = (1, 32, 64, 64)
     image2 = (1, 32, 32, 32)
     image3 = (1, 32, 16, 16)
- 
+
     image1 = torch.rand(image1)
     image2 = torch.rand(image2)
     image3 = torch.rand(image3)
@@ -368,6 +365,6 @@ if __name__ == "__main__":
     channel = (32, 32, 32)
     # Model
     head = Detect26_ASFFHead(nc=80, ch=channel)
- 
+
     out = head(image)
     print(out)
